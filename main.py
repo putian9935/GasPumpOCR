b48259af22387f7774d562ed276e17d658a7bb11
putian9935 <pl35@u.nus.edu>
2021-12-07 22:56:50 +0800

    add logger and banner

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from time import perf_counter, sleep
 import sys
 
 from matplotlib.pyplot import get
-from logger import Logger 
-from takeAndroidScreenshot import takeScreenshot 
+from logger import Logger
+from takeAndroidScreenshot import takeScreenshot
 
 from ImageProcessing import FrameProcessor
 
 from color_classification import ColorClassification
 from pre_process import preprocessing
+from datetime import timedelta
+
 
 def getDigits():
     """Get digit string from raw picture"""
@@ -21,34 +23,55 @@ def getDigits():
     rot_angle = 2
 
     frameProcessor = FrameProcessor(std_height, version)
-    preprocessor = lambda _ : preprocessing(_, roi, rot_angle)
-    
+    def preprocessor(_): return preprocessing(_, roi, rot_angle)
+
     def process_func(file_name):
         frameProcessor.set_image(file_name, preprocessor)
-        output = frameProcessor.process_image_plain(ColorClassification().convert_to_01)
-        return output 
-    return process_func 
+        output = frameProcessor.process_image_plain(
+            ColorClassification().convert_to_01)
+        return output
+    return process_func
+
 
 class Reader:
-    def __init__(self, initial_exponent, screenshot_buffer, sleep_init = 1):
+    def __init__(self, initial_exponent, screenshot_buffer, sleep_init=1):
         self.exponent = initial_exponent
         self.logger = Logger()
-        self.sleep_factor = 1.1 
+        self.sleep_factor = 1.1
         self.sleep_lag = sleep_init
         self.screenshot_buffer = screenshot_buffer
         self.get_digit = getDigits()
 
     def turn_on_read(self):
         last_digits = None
-        while True: 
+        while True:
             takeScreenshot(self.screenshot_buffer)
             new_digits = self.get_digit(self.screenshot_buffer)
-            print(new_digits)
-            if new_digits != last_digits:
+
+            if new_digits == last_digits:
                 self.sleep_lag *= self.sleep_factor
-            else: 
-                print(new_digits)
+            else:
+                pressure = self.cast_digits_to_number(last_digits, new_digits)
+                print('-'*81)
+                print("New pressure of %s since %s elapsed, " % (
+                    pressure,
+                    str(timedelta(seconds=perf_counter()))
+                ))
+                print("Current sleep lag is %d s" % int(self.sleep_lag))
+                print('-'*81)
+                self.logger.log([perf_counter(), pressure])
+                last_digits = new_digits
             sleep(self.sleep_lag)
+
+    def cast_digits_to_number(self, old_digits, new_digits):
+        """Given new and last digits, return the correct value
+        """
+        old = int(old_digits)
+        new = int(new_digits)
+        if abs(new - old) > 500:
+            self.exponent -= 1
+        return new * 10 ** self.exponent
+
 
 reader = Reader(-5, "./1.png")
 reader.turn_on_read()
--- takeAndroidScreenshot.py
+++ takeAndroidScreenshot.py
@@ -6,5 +6,5 @@ def takeScreenshot(filename="./1.png"):
     See https://stackoverflow.com/questions/20983351/taking-screenshot-on-emulator-from-android-studio
     """
     system("adb shell screencap -p /sdcard/screen.png")
-    system("adb pull /sdcard/screen.png %s"%filename)
+    system("adb pull /sdcard/screen.png %s>NUL"%filename)
     system("adb shell rm /sdcard/screen.png")
