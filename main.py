4d5eefe1193aea7b55d40d6097a4580092f91618
putian9935 <pl35@u.nus.edu>
2021-12-07 22:26:57 +0800

    add timed screenshots

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import cv2
 import time
+from time import perf_counter, sleep
 import sys
+
+from matplotlib.pyplot import get
+from logger import Logger 
+from takeAndroidScreenshot import takeScreenshot 
 
 from ImageProcessing import FrameProcessor
 
@@ -23,3 +28,23 @@ def getDigits():
         output = frameProcessor.process_image_plain(ColorClassification().convert_to_01)
         return output 
     return process_func 
+
+class Reader:
+    def __init__(self, initial_exponent, screenshot_buffer, sleep_init = 1):
+        self.exponent = initial_exponent
+        self.logger = Logger()
+        self.sleep_factor = 1.1 
+        self.sleep_lag = sleep_init
+        self.screenshot_buffer = screenshot_buffer
+        self.get_digit = getDigits()
+
+    def turn_on_read(self):
+        last_digits = None
+        while True: 
+            takeScreenshot(self.screenshot_buffer)
+            new_digits = self.get_digit(self.screenshot_buffer)
+            if new_digits != last_digits:
+                self.sleep_lag *= self.sleep_factor
+            else: 
+                print(new_digits)
+            sleep(self.sleep_lag)
---
+++ takeAndroidScreenshot.py
@@ -0,0 +1,10 @@
+from os import system 
+
+def takeScreenshot(filename="./1.png"):
+    """Take Screenshot with adb (assume in PATH)
+
+    See https://stackoverflow.com/questions/20983351/taking-screenshot-on-emulator-from-android-studio
+    """
+    system("adb shell screencap -p /sdcard/screen.png")
+    system("adb pull /sdcard/screen.png %s"%filename)
+    system("adb shell rm /sdcard/screen.png")
