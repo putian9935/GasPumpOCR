037078d1242f7e4e1f9e0c67b819477192b25510
StevenTouzard <steven.touzard@nus.edu.sg>
2021-12-12 22:23:22 +0800

    fix dark mode

--- ImageProcessing/erode.py
+++ ImageProcessing/erode.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-def remove_bridge(img):    
+def remove_bridge(img):
     kernel = np.ones((2, 2), np.uint8)
     erosion = cv2.dilate(img, kernel, iterations=10)
-    erosion[75:86,55:72] = 255
-    
+
+    erosion[70:80,45:68, ] = 255
+
     return (cv2.erode(erosion, kernel, iterations=5)[...,0] == 0).astype(np.uint8)
-    
